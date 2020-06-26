3030777bf3ac8bcf7b81762d80e3f2538fc21821
Boris Lukianov <boris.lukianov@seniordev.com>
2020-06-26 16:49:39 +0300

    AWS - error_rate_exceeded alarm

--- terraform/api/alarm.tf
+++ terraform/api/alarm.tf
@@ -56,3 +56,51 @@ resource "aws_cloudwatch_metric_alarm" "no_healthy_hosts" {
     LoadBalancer = aws_lb.front_end.arn_suffix
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "error_rate_exceeded" {
+  alarm_name                = "tf-${var.service_instance_name}-error-rate-exceeded"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "2"
+  threshold                 = "10"
+  alarm_description         = "Request error rate has exceeded 10%"
+  insufficient_data_actions = []
+
+  metric_query {
+    id          = "e1"
+    expression  = "m2/m1*100"
+    label       = "Error Rate"
+    return_data = "true"
+  }
+
+  metric_query {
+    id = "m1"
+
+    metric {
+      metric_name = "RequestCount"
+      namespace   = "AWS/ApplicationELB"
+      period      = "120"
+      stat        = "Sum"
+      unit        = "Count"
+
+      dimensions = {
+        LoadBalancer = "app/web"
+      }
+    }
+  }
+
+  metric_query {
+    id = "m2"
+
+    metric {
+      metric_name = "HTTPCode_ELB_5XX_Count"
+      namespace   = "AWS/ApplicationELB"
+      period      = "120"
+      stat        = "Sum"
+      unit        = "Count"
+
+      dimensions = {
+        LoadBalancer = "app/web"
+      }
+    }
+  }
+}
