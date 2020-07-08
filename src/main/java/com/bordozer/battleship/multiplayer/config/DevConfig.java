5219b394c6c7b15a3b600135615937ebd46a30f4
Boris Lukianov <boris.lukianov@seniordev.com>
2020-07-08 20:52:36 +0300

    selfsigned strategy

--- src/main/java/com/bordozer/battleship/multiplayer/config/DevConfig.java
+++ src/main/java/com/bordozer/battleship/multiplayer/config/DevConfig.java
@@ -1,11 +1,10 @@
 package com.bordozer.battleship.multiplayer.config;
 
-import com.bordozer.commons.utils.FileUtils;
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContextBuilder;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,23 +12,18 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
-import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
 
 @Configuration
 @Profile(value = "dev")
 public class DevConfig {
 
-    @Value("${server.ssl.key-store-password}")
-    private String trustStorePassword;
-
     @Bean
-    public RestTemplate restTemplate() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException, KeyManagementException {
+    public RestTemplate restTemplate() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
         final SSLContext sslContext = new SSLContextBuilder()
-                .loadTrustMaterial(FileUtils.getResourceFile("keystore/battleship.p12"), trustStorePassword.toCharArray())
+                .loadTrustMaterial(new TrustSelfSignedStrategy())
                 .build();
         final SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
         final HttpClient httpClient = HttpClients.custom()
