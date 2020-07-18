1d69919fdad6dd90d696b1247cedcec2184b163f
Boris Lukianov <boris.lukianov@seniordev.com>
2020-07-18 10:38:08 +0300

    https dev as default

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./gradlew clean build -x test && java -ea -Dspring.profiles.active=dev -jar build/libs/battleship-game.jar
+./gradlew clean buildUI  build -x test && java -ea -Dspring.profiles.active=dev-https -jar build/libs/battleship-game.jar
 #./gradlew clean buildUI build -x check
 #./gradlew clean buildUI build -x check && java -ea -Dspring.profiles.active=dev -jar build/libs/battleship-game.jar
