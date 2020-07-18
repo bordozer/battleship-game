0f4fcb3c9567d4737385a11988d040309a4cca76
Boris Lukianov <boris.lukianov@seniordev.com>
2020-07-18 13:51:26 +0300

    Games layout 3

--- src/main/webapp/src/router.js
+++ src/main/webapp/src/router.js
@@ -1,10 +1,10 @@
 /* jshint esversion: 6 */
-import React from "react";
-import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import React from 'react';
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faPlusSquare} from '@fortawesome/free-regular-svg-icons';
-import {faExternalLinkAlt} from '@fortawesome/free-solid-svg-icons';
+import {faLaptop, faList} from '@fortawesome/free-solid-svg-icons';
 
 import GamesPage from 'src/pages/games';
 import BattlePage from 'src/pages/battle';
@@ -16,36 +16,34 @@ export default function AppRouter() {
             <div>
                 <div className="row bg-light">
 
-                    <div className="col-4 text-center">
-                        <div className="row">
-                            <div className="col-2">
-                                <Link to="/battle?gameId=" style={{textDecoration: 'none'}}>
-                                    <FontAwesomeIcon icon={faPlusSquare} className='fa-2x' title='Create new multiplayer game'/>
-                                </Link>
-                            </div>
-                            <div className="col-10">
-                                <Link to="/" style={{textDecoration: 'none'}}>
-                                    Multiplayer games
-                                </Link>
-                            </div>
+                    <div className="col-6">
+                        <h4>Battleship game</h4>
+                    </div>
+
+                    <div className="col-6">
+                        <div className='ml-10 toolbar-buttons'>
+                            <a href="https://bordozer.github.io/" target="_">
+                                <FontAwesomeIcon
+                                    icon={faLaptop}
+                                    className='fa-2x'
+                                    title='Single player game (external link, opens in a new window)'/>
+                            </a>
                         </div>
-
-                    </div>
-
-                    <div className="col-4 text-center">
-                        Battleship game
-                    </div>
-
-                    <div className="col-4 text-center">
-                        <div className="row">
-                            <div className="col-10">
-                                Single player
-                            </div>
-                            <div className="col-2">
-                                <a href="https://bordozer.github.io/" target="_">
-                                    <FontAwesomeIcon icon={faExternalLinkAlt} className='fa-2x' title='Open single player (external link)'/>
-                                </a>
-                            </div>
+                        <div className='ml-10 toolbar-buttons'>
+                            <Link to="/battle?gameId=" style={{textDecoration: 'none'}}>
+                                <FontAwesomeIcon
+                                    icon={faPlusSquare}
+                                    className='fa-2x'
+                                    title='Create new multiplayer game'/>
+                            </Link>
+                        </div>
+                        <div className='ml-10 toolbar-buttons'>
+                            <Link to="/" style={{textDecoration: 'none'}}>
+                                <FontAwesomeIcon
+                                    icon={faList}
+                                    className='fa-2x'
+                                    title='Multiplayer games'/>
+                            </Link>
                         </div>
                     </div>
                 </div>
